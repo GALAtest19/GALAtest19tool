9c6e4d88ff46c03594f93e475b27e804b0d7046a
GALAtest19 <galaewan70@gmail.com>
2024-10-26 09:04:13 +0500

    Update cpmgala.py

--- cpmgala.py
+++ cpmgala.py
@@ -2,7 +2,7 @@ import requests
 from time import sleep
 BASE_URL: str = "https://red-viper-815239.hostingersite.com/api"
 
-class cpmgala:
+class CPMGala:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
